a2cd669baae8fc8f74a648b2b7c8eeea6e172c44
Samir Ahmed <samirahmed2013@gmail.com>
2012-12-01 22:28:11 -0500

    Added Protect Admin Page

--- scoreboard.rb
+++ scoreboard.rb
@@ -21,6 +21,12 @@ class Question
   property :answer,   String
 end
 
+class Competition
+  include DataMapper::Resource
+  property   :id,     Serial
+  property :active, Boolean, :default => false;
+end
+
 DataMapper.auto_upgrade!
 
 def get_user
@@ -47,13 +53,19 @@ get "/login" do
 end
 
 get "/" do
+
   @user = get_user 
   @message = session["message"]
   session["message"] = nil
   
   @competitors = Competitor.all
   @total = 100
-  erb :index
+  
+  if active!
+    erb :index
+  else
+    "No Competition"
+  end
 end
 
 get '/answer' do
@@ -105,4 +117,38 @@ post '/answer' do
   end
   
   redirect to_answer_page
+end
+
+get '/admin/disable' do
+  protected!
+  Competition.all.destroy
+  "Deactivated Competition"
+end
+
+get '/admin/start' do
+  protected!
+  comp = Competition.first_or_create({ :id => 1 })
+  comp.active = true;
+  comp.save;
+  "Activated Competition"
+end
+
+helpers do
+
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV["ADMIN_USERNAME"] || 'admin', ENV["ADMIN_PASSWORD"] || 'admin' ]
+  end
+
+end 
+
+def active!
+  Competition.all.map{|c| c.active }.any?
 end
